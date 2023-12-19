b9442c05bad9bba273ec0688fc1800f5143ebf63
Lucas Oliveira <lucas.devmobile@gmail.com>
2023-12-19 13:09:20 -0300

    same

--- exercicios/ex22.py
+++ exercicios/ex22.py
@@ -4,9 +4,11 @@
 #quantas letras ao todo( sem considerar espacos)
 #quantas letras tem o primeiro nome
 
-pessoa=str(input('Digite o nome da pessoa '))
-print (pessoa.upper())
-print (pessoa.lower())
-print (len (pessoa))
-print (len(pessoa[0:6]))
+nome=str(input('Digite o nome da pessoa ')).strip()    #o .strip exclui espacos digitados sem querer
+print('Analisando seu nome...')
+print('Seu nome em maiusculas eh {} '.format(nome.upper()))
+print('Seu nome em minusculas eh {} '.format(nome.lower()))
+print('Seu nome tem ao todo {} letras '.format(len(nome)-nome.count(' ')))  # -nome.count tira os espacos do meio
+print('Seu primeiro nome tem {} letras '.format(nome.find(' '))) # .find ' ' acha o primeiro espaco entre os nomes
+
 
