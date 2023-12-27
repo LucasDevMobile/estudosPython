ee92d63238b7d3b6fefb6fc32538068b4d375fd9
Lucas Oliveira <lucas.devmobile@gmail.com>
2023-12-27 12:30:16 -0300

    ex

--- exercicios/ex38.py
+++ exercicios/ex38.py
@@ -1,2 +1,11 @@
 #escreva um programa que leia dois numeros inteiros e compare-os mostrando na tela uma mesnsagem
 #o primeiro valor eh maior,o segundo valor eh maior,nao existe valor maior,os dois sao iguais
+
+n1=int(input('Digite o primeiro numero '))
+n2=int(input('Digite o segundo numero '))
+
+if n1>n2:
+    print('O primeiro numero eh maior ')
+elif n2>n1:
+    print('O segundo numero eh maior ')
+else: print('Nao existe numero maior, os dois sao iguais! ')
