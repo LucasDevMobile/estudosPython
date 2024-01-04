da449a20819c48d70ce805566d6107480bdc69a5
Lucas oliveira <lucasmobiledev@gmail.com>
2024-01-04 00:31:28 -0300

    exend

--- exercicios/dsds.py
+++ exercicios/dsds.py
@@ -1,17 +1,6 @@
-contvelho=0
-for c in range (1,3):
-
-
-
-    nome=str(input('Qual o seu nome:'))
-    idade=int(input('Qual a sua idade?'))
-    sexo=(input('Qual o seu sexo? Digite m para masculino ou f para feminino:'))
-
-    if sexo=='m':
-        contvelho=contvelho+idade
-print(contvelho)
-        
-       
-        
-
-        
+inicio=int(input('inicio '))
+fim=int(input('fim '))
+passo=int(input('passo '))   # le os dados do usuario e printa (passo= de qts em qts)
+for num in range(inicio, fim+1, passo):
+    print(num)
+print('fim')
