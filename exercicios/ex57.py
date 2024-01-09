53e2b7e612f65cb1e80874cd450bac5b2ed21309
Lucas oliveira <lucasmobiledev@gmail.com>
2024-01-09 00:08:38 -0300

    exx

--- exercicios/ex57.py
+++ exercicios/ex57.py
@@ -3,5 +3,12 @@
 sexo=str(input('Qual o seu sexo? ')).upper()
 
 while sexo!= 'F' and sexo != 'M':
-    print('Digite novamente: ')
+    print('Digite uma opcao valida : ')
     sexo=str(input('Qual o seu sexo? ')).upper()
+M='Masculino'
+F='Feminino'
+if sexo=='M':
+    print('Obrigado, voce eh do sexo {}'.format(M))
+else:
+    print('Obrigado, voce eh do sexo {}'.format(F))
+
