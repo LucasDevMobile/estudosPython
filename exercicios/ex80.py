96e57d8a7f43ca6905b835d4440228bd19043315
Lucas Oliveira <lucas.devmobile@gmail.com>
2024-01-23 10:12:01 -0300

    fimex

--- exercicios/ex80.py
+++ exercicios/ex80.py
@@ -1,15 +1,19 @@
-#crie um programa para inserir 5 valores em ordem
+# crie um programa para inserir valores na ordem certa
 
 lista=[]
 
-valor=int(input('Digite um valor:'))
-lista.append(valor)
-valor=int(input('Digite um valor:'))
-lista.append(valor)
-
-valor=int(input('Digite um valor:'))
-
-if valor < max(lista) in lista:
-    lista.insert(0,valor)
+for c in range (0,5):
+    n=int(input('Digite um valor:'))
+    if c==0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        posicao=0
+        while posicao < len(lista):
+            if n<= lista[posicao]:
+                lista.insert(posicao,n)
+                break
+            posicao+=1
+            
+print(f'Os valores digitados em ordem foram {lista}')
     
-print(lista)
+       
--- exercicios/ex83.py
+++ exercicios/ex83.py
@@ -1,3 +1,20 @@
 #crie um programa q digite uma expressao q use parenteses
 #devera analisar se a expressao esta com parenteses abertos e fechados na ordem correta
 # ex: ((a+b)*(a*c)-2) errado
+
+expressao = input("Digite uma expressão com parênteses: ")
+pilha = []
+
+for caractere in expressao:
+    if caractere == '(':
+        pilha.append(caractere)
+    elif caractere == ')':
+        if not pilha:
+            print("Os parênteses não estão balanceados.")
+            break  # Se encontrou ')' sem correspondente '('
+        pilha.pop()
+
+if not pilha:
+    print("Os parênteses estão balanceados.")
+else:
+    print("Os parênteses não estão balanceados.")
