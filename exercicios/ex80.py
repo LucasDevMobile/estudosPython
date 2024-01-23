f6770b41dda4e6031d66de030078ebd82819e013
Lucas oliveira <lucasmobiledev@gmail.com>
2024-01-22 23:52:49 -0300

    fimex

--- exercicios/ex80.py
+++ exercicios/ex80.py
@@ -1,15 +1,21 @@
-#crie um programa para inserir 5 valores em ordem
+# Cria uma lista vazia para armazenar os valores
+lista_ordenada = []
 
-lista=[]
+# Loop para inserir 5 valores
+for i in range(5):
+    # Solicita ao usuário para inserir um valor
+    valor = int(input(f"Insira o {i + 1}º valor: "))
 
-valor=int(input('Digite um valor:'))
-lista.append(valor)
-valor=int(input('Digite um valor:'))
-lista.append(valor)
+    # Encontra a posição correta para inserir o valor na lista
+    posicao = 0
+    while posicao < len(lista_ordenada) and lista_ordenada[posicao] < valor:
+        posicao += 1
 
-valor=int(input('Digite um valor:'))
+    # Insere o valor na posição correta
+    lista_ordenada.insert(posicao, valor)
 
-if valor < max(lista) in lista:
-    lista.insert(0,valor)
-    
-print(lista)
+    # Exibe a posição em que o valor foi inserido
+    print(f"O valor {valor} foi inserido na posição {posicao + 1} da lista.")
+
+# Exibe a lista ordenada
+print("Lista ordenada:", lista_ordenada)
---
+++ exercicios/ex83.py
@@ -0,0 +1,3 @@
+#crie um programa q digite uma expressao q use parenteses
+#devera analisar se a expressao esta com parenteses abertos e fechados na ordem correta
+# ex: ((a+b)*(a*c)-2) errado
