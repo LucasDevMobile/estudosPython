58b99a5a543f1d9cd293e82f5402f597d7b3f279
Lucas Oliveira <lucas.devmobile@gmail.com>
2024-01-23 09:43:26 -0300

    e

--- exercicios/ex80.py
+++ exercicios/ex80.py
@@ -1,15 +1,13 @@
-#crie um programa para inserir 5 valores em ordem
+#crie um programa para inserir 5 valores em ordem 
 
 lista=[]
 
-valor=int(input('Digite um valor:'))
-lista.append(valor)
-valor=int(input('Digite um valor:'))
-lista.append(valor)
-
-valor=int(input('Digite um valor:'))
-
-if valor < max(lista) in lista:
+while True:
+    valor=int(input('Digite um valor:'))
     lista.insert(0,valor)
     
-print(lista)
+        
+        
+        
+        
+    print(lista)
