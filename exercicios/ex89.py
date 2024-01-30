5633f29be9d4e74e32195844df809e442dbb9d92
Lucas oliveira <lucasmobiledev@gmail.com>
2024-01-29 22:27:48 -0300

    fimex

--- exercicios/ex89.py
+++ exercicios/ex89.py
@@ -2,19 +2,28 @@
 #perguntar se quer continuar
 #mostrar media a acessar nome do aluno pra saber qual foram as duas notas
 
-lista=[]
+ficha=[]
 
 while True:
 
     aluno=str(input('Digite o nome:'))
-    nota=float(input('Digite a primeira nota:'))
+    nota1=float(input('Digite a primeira nota:'))
     nota2=float(input('Digite a primeira nota:'))
-    lista.append(aluno)
-    lista.append(nota)
-    lista.append(nota2)
+    media=(nota1+nota2) / 2
+
+    ficha.append([aluno,[nota1,nota2],media])
+    
     continuar=str(input('Deseja continuar [S/N]')).upper()
     if continuar=='N':
         break
-print(lista)
 
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+
+while True:
+    opc=int(input('Mostrar notas de qual aluno? (999 para interromper):'))
+    if opc==999:
+        break
+    if opc<=len(ficha)-1:
+        print(f'Notas de {ficha[opc][0]} sao {ficha[opc][1] }')
     
