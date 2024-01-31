e9353882b9f6f0b036f5d69bbd6daef04a25c399
Lucas oliveira <lucasmobiledev@gmail.com>
2024-01-30 23:33:30 -0300

    d

--- exercicios/asdf.py
+++ exercicios/asdf.py
@@ -1,21 +1,4 @@
-temp=[]
-lista=[]
-
-
-for c  in range(0,7):
-    temp.append(int(input('Digite um valor:')))
-    
-for c in temp:
-    if c % 2 == 0:
-        lista.append(c)
-        lista.sort()
-      
-    
-
-    
-    
-     
-    
-
-print(f'Os numeros pares sao [{lista}]')
-print(f'Os numeros impares sao [{lista}]')
+dic={'nome':'pedro','idade':25,'sexo':'masculino'}
+for k,v in dic.items():
+    print(f'o {k} e {v}')
+    print(f'o {v} e {k}')
