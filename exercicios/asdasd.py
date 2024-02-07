c3598cc815c4ababfa9cca3a80e7a34d7741b022
Lucas oliveira <lucasmobiledev@gmail.com>
2024-02-06 22:31:04 -0300

    e

---
+++ exercicios/asdasd.py
@@ -0,0 +1,12 @@
+pessoas = {'nome':'Lucas','idade':35,'sexo':'M'}
+
+
+lista=[]
+
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+
+lista.append(pessoas.copy())
+print(lista)
+
