9eb7966c604b7a84d22073b62522542e058118fd
Lucas Oliveira <lucas.devmobile@gmail.com>
2024-02-09 12:52:32 -0300

    ex92

---
+++ exercicios/ex93.py
@@ -0,0 +1,15 @@
+#crie um programa que gerencie um jogador.o programa va ler nome e qts partidas ele jogou
+#qts gols na partida 1,2,3,4
+#depois vai ler quantidade de gols em cada partida
+#guarde em um dicionario incluindo total de gols feitos no campeonato
+
+#{'nome':zico,'gols':[3,0,2,1,4],'total':10}
+
+#o campo nome tem o valor zico
+#o campo gols tem o valor [3,0,2,1,4]
+#o campo total tem o valor 10
+
+#o jogador zico jogou 5 partidas
+#na partida 0 fez 3 gols
+#na partida 1 fez 0 gols....etc
+
