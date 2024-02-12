532b1a2c79a95bb18b465185824ec1fbc4332f30
Lucas oliveira <lucasmobiledev@gmail.com>
2024-02-12 13:33:15 -0300

    ex

--- exercicios/ex91.py
+++ exercicios/ex91.py
@@ -3,7 +3,7 @@
 
 from random import randint
 
-# Resultados dos dados para cada jogador
+
 resultados = {}
 
 resultados['jogador1'] = randint(1, 6)
@@ -11,17 +11,17 @@ resultados['jogador2'] = randint(1, 6)
 resultados['jogador3'] = randint(1, 6)
 resultados['jogador4'] = randint(1, 6)
 
-# Imprime os resultados de cada jogador
+
 for jogador, resultado in resultados.items():
     print(f'{jogador} tirou {resultado}')
 
-# Calcula a pontuação máxima
+
 pontuacao_maxima = max(resultados.values())
 
-# Identifica os jogadores que obtiveram a pontuação máxima
+
 vencedores = [jogador for jogador, resultado in resultados.items() if resultado == pontuacao_maxima]
 
-# Imprime o vencedor ou vencedores
+
 if len(vencedores) == 1:
     print(f'O vencedor é o {vencedores[0]} com {pontuacao_maxima} pontos!')
 else:
